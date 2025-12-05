922748deff05548859e21f5df76ed988da1c6c1a
Jovany Leandro G.C <bit4bit@riseup.net>
2025-12-05 23:16:19 +0200

    improv

--- raylib/lib/raylib.ex
+++ raylib/lib/raylib.ex
@@ -4,25 +4,25 @@ defmodule Raylib do
   """
 
   use Zig,
-      otp_app: :raylib,
-      c: [include_dirs: "/usr/local/include", link_lib: {:system, "raylib"}]
+    otp_app: :raylib,
+    c: [include_dirs: "/usr/local/include", link_lib: {:system, "raylib"}]
 
   ~Z"""
   const ray = @cImport({
-    @cInclude("raylib.h");
+      @cInclude("raylib.h");
   });
 
   pub const InitWindowOptions = struct { width: i32, height: i32, title: [*]const u8 };
   const beam = @import("beam");
 
   pub fn init_window(options: InitWindowOptions) beam.term {
-  ray.InitWindow(options.width, options.height, options.title);
-  return beam.make(.ok, .{});
+      ray.InitWindow(options.width, options.height, options.title);
+      return beam.make(.ok, .{});
   }
 
   pub fn close_window() beam.term {
-  ray.CloseWindow();
-  return beam.make(.ok, .{});
+      ray.CloseWindow();
+      return beam.make(.ok, .{});
   }
   """
 end
